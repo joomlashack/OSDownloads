ac6b8c11e1e9472e47b22ba357bc6d09628133bf
Anderson Grudtner Martins <anderson@ostraining.com>
2014-12-11 15:51:58 -0200

    Improve the popup layout
    Remove the list mark from messages and make the iframe transparent for Joomla 2.5.
    Fix the code style

--- src/site/views/item/tmpl/download.php
+++ src/site/views/item/tmpl/download.php
@@ -8,35 +8,42 @@
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die( 'Restricted access' );
 
-$mainframe    = JFactory::getApplication();
-$params       = clone($mainframe->getParams('com_osdownloads'));
+$app = JFactory::getApplication();
+$db  = JFactory::getDBO();
+
+$params       = clone($app->getParams('com_osdownloads'));
 $thankyoupage = $params->get("thankyoupage", "Thank you");
 
-$db = JFactory::getDBO();
-$query = "UPDATE `#__osdownloads_documents` SET downloaded = downloaded + 1 WHERE id = {$this->item->id}";
+$email      = trim(JRequest::getVar("email"));
+$emailRegex = "/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/";
+
+$query = $db->getQuery(true)
+    ->update('#__osdownloads_documents')
+    ->set('downloaded = downloaded + 1')
+    ->where("id = " . $db->q($this->item->id));
 $db->setQuery($query);
 $db->query();
-
 ?>
 
 <style>
-    body {
+    body,
+    div#all,
+    div#main {
         background: none transparent !important;
     }
 </style>
 
-<?php
-$email = trim(JRequest::getVar("email"));
-if ($this->item->require_email && !preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $email)) {
-?>
-<div class="error"><?php echo(JText::_("COM_OSDOWNLOADS_WRONG_EMAIL"));?></div>
-<?php
-} else {?>
-<div class="contentopen">
-    <h1 class="thank"><?php echo($thankyoupage);?></h1>
-    <p class="download_link"><?php echo JText::sprintf("COM_OSDOWNLOADS_CLICK_TO_DOWNLOAD_FILE", $this->download_url);?>
-        <meta http-equiv="refresh" content="0;url=<?php echo $this->download_url;?>">
-    </p>
+<div id="osdownloads-thankyou">
+    <?php if ($this->item->require_email && !preg_match($emailRegex, $email)) : ?>
+        <div class="error"><?php echo(JText::_("COM_OSDOWNLOADS_WRONG_EMAIL"));?></div>
+    <?php else : ?>
+        <div class="contentopen">
+            <h1 class="thank"><?php echo($thankyoupage);?></h1>
+            <p class="download_link">
+                <?php echo JText::sprintf("COM_OSDOWNLOADS_CLICK_TO_DOWNLOAD_FILE", $this->download_url);?>
+
+                <meta http-equiv="refresh" content="0;url=<?php echo $this->download_url;?>">
+            </p>
+        </div>
+    <?php endif; ?>
 </div>
-<?php
-}

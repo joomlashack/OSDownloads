e6fd9ba390a8ed71b03a4beef531c5990bf09b41
Anderson Grudtner Martins <anderson@ostraining.com>
2014-12-16 20:32:52 -0200

    Fix the file admin view after the latest commit

--- src/admin/views/file/view.html.php
+++ src/admin/views/file/view.html.php
@@ -3,58 +3,42 @@
  * @package   OSDownloads
  * @contact   www.alledia.com, hello@alledia.com
  * @copyright 2014 Alledia.com, All rights reserved
- * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
-// Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die( 'Restricted access' );
 
-jimport('joomla.application.component.view');
-jimport('legacy.model.legacy');
+defined('_JEXEC') or die;
 
-require_once __DIR__ . '/../../models/items.php';
+use Alledia\Framework\Factory;
 
-class OSDownloadsViewFiles extends JViewLegacy
+jimport('legacy.view.legacy');
+
+class OSDownloadsViewFile extends JViewLegacy
 {
-    public function __construct()
-    {
-        $model = JModelLegacy::getInstance('OSDownloadsModelItems');
-        $this->setModel($model, true);
-
-        $this->addTemplatePath(__DIR__ . '/tmpl');
-    }
 
     public function display($tpl = null)
     {
-        $app   = JFactory::getApplication();
-        $model = $this->getModel();
-        $db    = JFactory::getDBO();
+        JTable::addIncludePath(JPATH_COMPONENT.'/tables');
 
-        $pagination = $model->getPagination();
+        $mainframe  = JFactory::getApplication();
+        $cid = JRequest::getVar("cid");
 
-        $query = $model->getItemsQuery();
+        if (is_array($cid)) {
+            $cid = $cid[0];
+        }
+        $item = JTable::getInstance("document", "OsdownloadsTable");
+        $item->load($cid);
 
-        $db->setQuery($query, $pagination->limitstart, $pagination->limit);
-        $items = $db->loadObjectList();
-
-        $filterOrder    = $app->getUserStateFromRequest("com_osdownloads.document.filter_order", 'filter_order', 'doc.id', '');
-        $filterOrderDir = $app->getUserStateFromRequest("com_osdownloads.document.filter_order_Dir", 'filter_order_Dir', 'asc', 'word');
-
-        $lists = array();
-        $lists['order_Dir'] = $filterOrderDir;
-        $lists['order']     = $filterOrder;
-
-        $filter             = new stdClass;
-        $filter->search     = $app->getUserStateFromRequest('com_osdownloads.document.request.search', 'search');
-        $filter->categoryId = $app->getUserStateFromRequest('com_osdownloads.document.request.cate_id', 'flt_cate_id');
+        if ($item->description_1) {
+            $item->description_1 = $item->brief . "<hr id=\"system-readmore\" />" . $item->description_1;
+        } else {
+            $item->description_1 = $item->brief;
+        }
 
         // Load the extension
-        $extension = Alledia\Framework\Factory::getExtension('OSDownloads', 'component');
+        $extension = Factory::getExtension('OSDownloads', 'component');
         $extension->loadLibrary();
 
-        $this->assignRef('lists', $lists);
-        $this->assignRef("items", $items);
-        $this->assignRef("filter", $filter);
-        $this->assignRef("pagination", $pagination);
+        $this->assignRef("item", $item);
         $this->assignRef("extension", $extension);
 
         $this->addToolbar();
@@ -64,14 +48,9 @@ class OSDownloadsViewFiles extends JViewLegacy
 
     protected function addToolbar()
     {
-        JToolBarHelper::title(JText::_('COM_OSDOWNLOADS_FILES'));
-        JToolBarHelper::custom('file', 'new.png', 'new_f2.png', 'JTOOLBAR_NEW', false);
-        JToolBarHelper::custom('file', 'edit.png', 'edit_f2.png', 'JTOOLBAR_EDIT', true);
-        JToolBarHelper::custom('file.delete', 'delete.png', 'delete_f2.png', 'JTOOLBAR_DELETE', true);
-        JToolBarHelper::divider();
-        JToolBarHelper::custom('file.publish', 'publish.png', 'publish_f2.png', 'JTOOLBAR_PUBLISH', true);
-        JToolBarHelper::custom('file.unpublish', 'unpublish.png', 'unpublish_f2.png', 'JTOOLBAR_UNPUBLISH', true);
-        JToolBarHelper::divider();
-        JToolBarHelper::preferences('com_osdownloads', '450');
+        JToolBarHelper::title(JText::_('COM_OSDOWNLOADS_FILE'));
+        JToolBarHelper::save('file.save', 'JTOOLBAR_SAVE');
+        JToolBarHelper::apply('file.apply', 'JTOOLBAR_APPLY');
+        JToolBarHelper::cancel('cancel', 'JTOOLBAR_CANCEL');
     }
 }

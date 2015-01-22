2f1bdd953a1a542596a2cbf6e741b022d426b2d4
Anderson Grudtner Martins <anderson@ostraining.com>
2015-01-22 16:08:12 -0200

    Add missed comments to file headers

--- src/admin/views/files/tmpl/default.j2.php
+++ src/admin/views/files/tmpl/default.j2.php
@@ -1,7 +1,12 @@
 <?php
+/**
+ * @package   OSDownloads
+ * @contact   www.alledia.com, hello@alledia.com
+ * @copyright 2014 Alledia.com, All rights reserved
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
 
-// Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die( 'Restricted access' );
+defined('_JEXEC') or die;
 
 $listOrder = $this->lists['order'];
 $listDirn  = $this->lists['order_Dir'];

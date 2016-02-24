6d57191d1e997590ba6bfc2f4ccabeb36dd7f48c
Anderson Grudtner Martins <anderson@ostraining.com>
2016-02-24 11:39:15 -0300

    Add missed button for preferences

--- src/admin/library/Free/Joomla/View/Admin/Emails.php
+++ src/admin/library/Free/Joomla/View/Admin/Emails.php
@@ -90,5 +90,7 @@ class Emails extends LegacyView
     {
         JToolBarHelper::title(JText::_('COM_OSDOWNLOADS') . ': ' . JText::_('COM_OSDOWNLOADS_EMAILS'));
         JToolBarHelper::deleteList('Are you sure?', 'emails.delete');
+        JToolBarHelper::divider();
+        JToolBarHelper::preferences('com_osdownloads', '450');
     }
 }

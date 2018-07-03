cfdc19c52fbe106d3889f1812a6646abd1db35b3
Bill Tomczak <bill@ostraining.com>
2018-07-03 16:00:24 -0700

    Add missing table for uninstall

--- src/admin/sql/install/mysql/uninstall.sql
+++ src/admin/sql/install/mysql/uninstall.sql
@@ -1,3 +1,4 @@
 DROP TABLE IF EXISTS `#__osdownloads_documents`;
 DROP TABLE IF EXISTS `#__osdownloads_emails`;
+DROP TABLE IF EXISTS `#__osdownloads_download_log`;
 DELETE FROM `#__categories` WHERE `extension` = 'com_osdownloads';

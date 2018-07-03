aefba26373478920aa8c3c8b563d34955b60187a
Bill Tomczak <bill@ostraining.com>
2018-07-03 04:41:41 -0700

    Minor code cleanup

--- src/admin/helpers/osdownloads.php
+++ src/admin/helpers/osdownloads.php
@@ -6,7 +6,8 @@
  * @license   GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-use Alledia\OSDownloads\Free\Helper\Helper;
+use Alledia\OSDownloads\Free;
+use Alledia\OSDownloads\Pro;
 
 defined('_JEXEC') or die;
 
@@ -14,22 +15,19 @@ defined('_JEXEC') or die;
  * Backward compatibility for the helper. We moved it to improve inheritance between
  * Free and Pro versions. Some plugins still call this class, including com_files.
  *
- * @deprecated 1.8.0  Use the Alledia\OSDownloads\Free\Helper\Helper class instead.
  */
 
-// Joomla can load this file without call the component, specially while loading
-// the sidebar. So we need to include the autoloader and libraries.
 if (!defined('OSDOWNLOADS_LOADED')) {
     require_once dirname(__DIR__) . '/include.php';
 }
 
 if (class_exists('\\Alledia\\OSDownloads\\Pro\\Helper\\Helper')) {
-    class OSDownloadsHelper extends Alledia\OSDownloads\Pro\Helper\Helper
+    class OSDownloadsHelper extends Pro\Helper\Helper
     {
 
     }
 } else {
-    class OSDownloadsHelper extends Helper
+    class OSDownloadsHelper extends Free\Helper\Helper
     {
 
     }

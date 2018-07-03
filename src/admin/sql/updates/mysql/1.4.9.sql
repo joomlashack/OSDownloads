d8a0f8fb17bf95e7112a46474e840564b4c973b6
Bill Tomczak <bill@ostraining.com>
2018-07-03 13:53:45 -0700

    Update minimum previous version to 1.4.9

--- src/admin/sql/updates/mysql/1.4.9.sql
+++ src/admin/sql/updates/mysql/1.4.9.sql
@@ -1,1 +1,17 @@
-ALTER TABLE `#__osdownloads_documents` CHANGE `file_path` `file_path` VARCHAR(255)  CHARACTER SET utf8  COLLATE utf8_general_ci  NOT NULL  DEFAULT '';
+-- MySQL Workbench Synchronization
+-- Generated: 2018-07-03 13:43
+-- Model: New Model
+-- Version: 1.0
+-- Project: Name of the project
+-- Author: Bill Tomczak
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='';
+
+ALTER TABLE `#__osdownloads_documents`
+  CHANGE COLUMN `file_path` `file_path` VARCHAR(255) NOT NULL ;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;

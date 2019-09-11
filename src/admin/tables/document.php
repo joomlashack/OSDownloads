5c97736f220a6892ab5bcaa650bc5a5203852586
Bill Tomczak <bill@joomlashack.com>
2019-09-11 14:24:54 -0700

    Simplify document table class

--- src/admin/tables/document.php
+++ src/admin/tables/document.php
@@ -21,11 +21,80 @@
  * along with OSDownloads.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Joomla\String\StringHelper;
+
 defined('_JEXEC') or die();
 
-require_once 'abstractdocument.php';
-
-class OsdownloadsTableDocument extends OSDownloadsTableAbstractDocument
+class OsdownloadsTableDocument extends JTable
 {
+    protected $_columnAlias = array(
+        'title' => 'name',
+        'catid' => 'cate_id'
+    );
 
+    /**
+     * @var JEventDispatcher
+     */
+    protected $_dispatcher = null;
+
+    public function __construct(&$_db)
+    {
+        parent::__construct('#__osdownloads_documents', 'id', $_db);
+    }
+
+    public function store($updateNulls = false)
+    {
+        $date = JFactory::getDate();
+        $user = JFactory::getUser();
+
+        $this->modified_time = $date->toSql();
+
+        $isNew = !empty($this->id);
+        if ($isNew) {
+            // New document
+            $this->downloaded      = 0;
+            $this->created_time    = $date->toSql();
+            $this->created_user_id = $user->get('id');
+
+        } else {
+            $this->modified_user_id = $user->get('id');
+        }
+
+        if (empty($this->alias)) {
+            $this->alias = $this->name;
+        }
+        $this->alias = JApplicationHelper::stringURLSafe($this->alias);
+
+        $result = $this->trigger('onOSDownloadsBeforeSaveFile', array(&$this, $isNew)) !== false;
+        if ($result) {
+            $result = parent::store($updateNulls);
+
+            $this->trigger('onOSDownloadsAfterSaveFile', array($result, &$this));
+        }
+
+        return $result;
+    }
+
+    public function delete($pk = null)
+    {
+        // Trigger events to osdownloads plugins
+        $result = $this->trigger('onOSDownloadsBeforeDeleteFile', array(&$this, $pk)) !== false;
+        if ($result) {
+            $result = parent::delete($pk['id']);
+
+            $this->trigger('onOSDownloadsAfterDeleteFile', array($result, $this->id, $pk));
+        }
+
+        return $result;
+    }
+
+    protected function trigger($event, array $arguments)
+    {
+        if ($this->_dispatcher === null) {
+            $this->_dispatcher = JEventDispatcher::getInstance();
+            JPluginHelper::importPlugin('osdownloads');
+        }
+
+        return $this->_dispatcher->trigger($event, $arguments);
+    }
 }

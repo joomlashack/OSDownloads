1f9b239bb1a54cfb6026414ad401fc1aa210aa28
Bill Tomczak <bill@joomlashack.com>
2021-08-09 13:47:14 -0700

    Update Joomla 3 email list template for no results

--- src/admin/views/emails/tmpl/default.j3.php
+++ src/admin/views/emails/tmpl/default.j3.php
@@ -24,6 +24,7 @@
 use Alledia\OSDownloads\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Language;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
 
 defined('_JEXEC') or die();
@@ -52,89 +53,103 @@ $container     = Factory::getPimpleContainer();
         <?php echo $this->sidebar; ?>
     </div>
     <div id="j-main-container" class="span10">
-        <?php echo LayoutHelper::render('joomla.searchtools.default', ['view' => $this]); ?>
+        <?php
+        echo LayoutHelper::render('joomla.searchtools.default', ['view' => $this]);
 
-        <table class="adminlist table table-striped" style="width: 100%; border: none;">
-            <thead>
-            <tr>
-                <th class="hidden-phone">
-                    <?php echo HTMLHelper::_('grid.checkall'); ?>
-                </th>
-                <th class="has-context span6">
-                    <?php echo HTMLHelper::_(
-                        'searchtools.sort',
-                        'COM_OSDOWNLOADS_EMAIL',
-                        'email.email',
-                        $listDirection,
-                        $listOrder
-                    ); ?>
-                </th>
-                <?php if ($this->extension->isPro()) : ?>
-                    <?php echo $this->loadTemplate('pro_headers'); ?>
-                <?php endif; ?>
-                <th>
-                    <?php echo HTMLHelper::_(
-                        'searchtools.sort',
-                        'COM_OSDOWNLOADS_FILE',
-                        'doc.name',
-                        $listDirection,
-                        $listOrder
-                    ); ?>
-                </th>
-                <th>
-                    <?php echo HTMLHelper::_(
-                        'searchtools.sort',
-                        'COM_OSDOWNLOADS_CATEGORY',
-                        'cat.title',
-                        $listDirection,
-                        $listOrder
-                    ); ?>
-                </th>
-                <th>
-                    <?php echo HTMLHelper::_(
-                        'searchtools.sort',
-                        'COM_OSDOWNLOADS_DATE',
-                        'email.downloaded_date',
-                        $listDirection,
-                        $listOrder
-                    ); ?>
-                </th>
-                <th class="hidden-phone center">
-                    <?php echo HTMLHelper::_(
-                        'searchtools.sort',
-                        'COM_OSDOWNLOADS_ID',
-                        'email.id',
-                        $listDirection,
-                        $listOrder
-                    ); ?>
-                </th>
-            </tr>
-            </thead>
-
-            <tbody>
-            <?php foreach ($this->items as $i => $item) : ?>
-                <tr class="row<?php echo $i % 2; ?>">
-                    <td class="hidden-phone" style="width: 1%;">
-                        <?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
-                    </td>
-                    <td class="has-context span6"><?php echo($item->email); ?></td>
+        if ($this->items) : ?>
+            <table class="adminlist table table-striped" style="width: 100%; border: none;">
+                <thead>
+                <tr>
+                    <th class="hidden-phone">
+                        <?php echo HTMLHelper::_('grid.checkall'); ?>
+                    </th>
+                    <th class="has-context span6">
+                        <?php echo HTMLHelper::_(
+                            'searchtools.sort',
+                            'COM_OSDOWNLOADS_EMAIL',
+                            'email.email',
+                            $listDirection,
+                            $listOrder
+                        ); ?>
+                    </th>
                     <?php if ($this->extension->isPro()) : ?>
-                        <?php
-                        $this->item = $item;
-                        echo $this->loadTemplate('pro_columns');
-                        ?>
+                        <?php echo $this->loadTemplate('pro_headers'); ?>
                     <?php endif; ?>
-                    <td><?php echo($item->doc_name); ?></td>
-                    <td><?php echo($item->cate_name); ?></td>
-                    <td><?php echo(HTMLHelper::_('date', $item->downloaded_date, 'd-m-Y H:m:s')); ?></td>
-                    <td class="hidden-phone center"><?php echo($item->id); ?></td>
+                    <th>
+                        <?php echo HTMLHelper::_(
+                            'searchtools.sort',
+                            'COM_OSDOWNLOADS_FILE',
+                            'doc.name',
+                            $listDirection,
+                            $listOrder
+                        ); ?>
+                    </th>
+                    <th>
+                        <?php echo HTMLHelper::_(
+                            'searchtools.sort',
+                            'COM_OSDOWNLOADS_CATEGORY',
+                            'cat.title',
+                            $listDirection,
+                            $listOrder
+                        ); ?>
+                    </th>
+                    <th>
+                        <?php echo HTMLHelper::_(
+                            'searchtools.sort',
+                            'COM_OSDOWNLOADS_DATE',
+                            'email.downloaded_date',
+                            $listDirection,
+                            $listOrder
+                        ); ?>
+                    </th>
+                    <th class="hidden-phone center">
+                        <?php echo HTMLHelper::_(
+                            'searchtools.sort',
+                            'COM_OSDOWNLOADS_ID',
+                            'email.id',
+                            $listDirection,
+                            $listOrder
+                        ); ?>
+                    </th>
                 </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
+                </thead>
 
-        <?php echo $this->pagination->getListFooter(); ?>
+                <tbody>
+                <?php foreach ($this->items as $i => $item) : ?>
+                    <tr class="row<?php echo $i % 2; ?>">
+                        <td class="hidden-phone" style="width: 1%;">
+                            <?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
+                        </td>
+                        <td class="has-context span6"><?php echo($item->email); ?></td>
+                        <?php if ($this->extension->isPro()) : ?>
+                            <?php
+                            $this->item = $item;
+                            echo $this->loadTemplate('pro_columns');
+                            ?>
+                        <?php endif; ?>
+                        <td><?php echo($item->doc_name); ?></td>
+                        <td><?php echo($item->cate_name); ?></td>
+                        <td><?php echo(HTMLHelper::_('date', $item->downloaded_date, 'd-m-Y H:m:s')); ?></td>
+                        <td class="hidden-phone center"><?php echo($item->id); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
 
+            <?php echo $this->pagination->getListFooter(); ?>
+
+        <?php else : ?>
+            <div class="alert alert-no-items">
+                <?php
+                if ($this->activeFilters || $this->state->get('filter.search')) :
+                    echo Text::_('COM_OSDOWNLOADS_EMAILS_NO_RESULTS');
+                else :
+                    echo Text::_('COM_OSDOWNLOADS_EMAILS_NONE');
+                endif;
+                ?>
+            </div>
+
+        <?php endif; ?>
         <input type="hidden" name="task" value=""/>
         <input type="hidden" name="boxchecked" value="0"/>
         <?php echo HTMLHelper::_('form.token'); ?>

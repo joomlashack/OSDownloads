e6bfceb2dffc1640c3e73a2035175df5bbe3cded
Bill Tomczak <bill@joomlashack.com>
2021-08-09 14:26:33 -0700

    Add useful comment

--- src/admin/include.php
+++ src/admin/include.php
@@ -55,6 +55,7 @@ if (defined('ALLEDIA_FRAMEWORK_LOADED') && !defined('OSDOWNLOADS_LOADED')) {
     Helper::loadLibrary('com_osdownloads');
 
     if (version_compare(Version::MAJOR_VERSION, '4', 'lt')) {
+        // Joomla 3 shims
         JLoader::register('ContentHelperRoute', JPATH_SITE . '/components/com_content/helpers/route.php');
     }
 

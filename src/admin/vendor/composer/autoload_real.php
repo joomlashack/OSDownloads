e48e78e31dd2761d0b9d86817707f0ebe9d97aaa
Bill Tomczak <bill@joomlashack.com>
2023-06-13 13:48:46 -0700

    Update all composer dependencies

--- src/admin/vendor/composer/autoload_real.php
+++ src/admin/vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit30e37e4406280450bdcfd6fc092dd33b
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit30e37e4406280450bdcfd6fc092dd33b::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire30e37e4406280450bdcfd6fc092dd33b($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit30e37e4406280450bdcfd6fc092dd33b::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire30e37e4406280450bdcfd6fc092dd33b($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }

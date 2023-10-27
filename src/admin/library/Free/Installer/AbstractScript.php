7d494bc07a5db204a0966612107ea15c38fdee57
Bill Tomczak <bill@joomlashack.com>
2023-10-27 16:15:53 -0700

    Enforce minimum v1.13.0 for updates

--- src/admin/library/Free/Installer/AbstractScript.php
+++ src/admin/library/Free/Installer/AbstractScript.php
@@ -29,9 +29,7 @@ use Joomla\CMS\Filesystem\Folder;
 use Joomla\CMS\Installer\InstallerAdapter;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Table\Category;
-use Joomla\CMS\Table\Extension;
 use Joomla\CMS\Table\Table;
-use Joomla\Registry\Registry;
 
 // phpcs:disable PSR1.Files.SideEffects
 defined('_JEXEC') or die();
@@ -52,11 +50,7 @@ class AbstractScript extends AllediaAbstractScript
         if ($type != 'uninstall') {
             $include = JPATH_ADMINISTRATOR . '/components/com_osdownloads/include.php';
             if (is_file($include) && include $include) {
-                $this->checkParamStructure();
                 $this->checkAndCreateDefaultCategory();
-                $this->fixOrderingParamForMenus();
-                $this->fixDownloadsViewParams();
-                $this->fixItemViewParams();
                 $this->fixDatabase();
                 $this->clearProData();
 
@@ -80,7 +74,7 @@ class AbstractScript extends AllediaAbstractScript
             ->from('#__categories')
             ->where([
                 'extension = ' . $db->quote('com_osdownloads'),
-                'published >= 0'
+                'published >= 0',
             ]);
 
         $total = (int)$db->setQuery($query)->loadResult();
@@ -94,7 +88,7 @@ class AbstractScript extends AllediaAbstractScript
                 'parent_id' => 1,
                 'extension' => 'com_osdownloads',
                 'published' => 1,
-                'language'  => '*'
+                'language'  => '*',
             ];
 
             $row->setLocation($data['parent_id'], 'last-child');
@@ -118,7 +112,7 @@ class AbstractScript extends AllediaAbstractScript
                         '(%1$s IS NULL OR %1$s = %2$s)',
                         $db->quoteName('language'),
                         $db->quote('')
-                    )
+                    ),
                 ]);
             $db->setQuery($query)->execute();
 
@@ -128,7 +122,7 @@ class AbstractScript extends AllediaAbstractScript
                 ->from('#__categories')
                 ->where([
                     $db->quoteName('extension') . '=' . $db->quote('com_osdownloads'),
-                    $db->quoteName('path') . '=' . $db->quote('')
+                    $db->quoteName('path') . '=' . $db->quote(''),
                 ]);
 
             if ($ids = $db->setQuery($query)->loadColumn()) {
@@ -140,186 +134,6 @@ class AbstractScript extends AllediaAbstractScript
                 }
             }
         }
-    }
-
-    /**
-     * Fix old values for the ordering param in menus, adding the table prefix.
-     *
-     * @return void
-     */
-    protected function fixOrderingParamForMenus()
-    {
-        $db = $this->dbo;
-
-        $query = $db->getQuery(true)
-            ->select('link')
-            ->select('params')
-            ->select('id')
-            ->from('#__menu')
-            ->where('link = ' . $db->quote('index.php?option=com_osdownloads&view=downloads'));
-        $db->setQuery($query);
-        $menus = $db->loadObjectList();
-
-        if ($menus) {
-            foreach ($menus as $menu) {
-                $params          = new Registry($menu->params);
-                $legacyOrderings = [
-                    'ordering',
-                    'name',
-                    'downloaded',
-                    'created_time',
-                    'modified_time'
-                ];
-
-                $ordering = $params->get('ordering');
-                if (in_array($ordering, $legacyOrderings)) {
-                    $params->set('ordering', 'doc.' . $ordering);
-
-                    $query = $db->getQuery(true)
-                        ->update('#__menu')
-                        ->set('params = ' . $db->quote($params->toString()))
-                        ->where('id = ' . $menu->id);
-
-                    $db->setQuery($query)->execute();
-                }
-            }
-        }
-    }
-
-    /**
-     * Detect legacy settings and fix the downloads view params. If legacy data
-     * is found, warn the user. We call legacy data, the param category_id with
-     * multiple values, in the downloads view. Represented issues for SEF
-     * URLs, so we refactored allowing only one category.
-     *
-     * @return void
-     */
-    protected function fixDownloadsViewParams()
-    {
-        $db = $this->dbo;
-
-        // Look for menu items for Category view
-        $query = $db->getQuery(true)
-            ->select([
-                'id',
-                'params',
-            ])
-            ->from('#__menu')
-            ->where('link = ' . $db->quote('index.php?option=com_osdownloads&view=downloads'));
-
-        $menuList = $db->setQuery($query)->loadObjectList();
-        if ($menuList) {
-            foreach ($menuList as $menu) {
-                $params = new Registry($menu->params);
-
-                // Does it have the old param and multiple categories selected?
-                $categories = (array)$params->get('category_id');
-                if (count($categories) > 1) {
-                    // Get the first category for the new param. If empty, use 0, the root category
-                    $categoryId = (int)array_shift($categories);
-                    $params->remove('category_id');
-
-                    // Update the link adding the selected category
-                    $link = 'index.php?option=com_osdownloads&view=downloads&id=' . $categoryId;
-
-                    $query = $db->getQuery(true)
-                        ->update('#__menu')
-                        ->where('id = ' . (int)$menu->id)
-                        ->set('link = ' . $db->quote($link))
-                        ->set('params = ' . $db->quote($params->toString()));
-                    $db->setQuery($query)->execute();
-
-                    $this->sendMessage(
-                        Text::sprintf('COM_OSDOWNLOADS_ERROR_INSTALLER_CATEGORY', $menu->id),
-                        'warning'
-                    );
-                }
-            }
-        }
-    }
-
-    /**
-     * Detect legacy settings and fix the item view params
-     *
-     * @return void
-     */
-    protected function fixItemViewParams()
-    {
-        $db = $this->dbo;
-
-        // Look for menu items for Item view
-        $query = $db->getQuery(true)
-            ->select([
-                'id',
-                'params',
-            ])
-            ->from('#__menu')
-            ->where('link = ' . $db->quote('index.php?option=com_osdownloads&view=item'));
-
-        $menuList = $db->setQuery($query)->loadObjectList();
-        if ($menuList) {
-            foreach ($menuList as $menu) {
-                $params = new Registry($menu->params);
-
-                // Does it have the old param?
-                $documentId = (int)$params->get('document_id');
-                if ($documentId) {
-                    $params->remove('document_id');
-
-                    // Update the link adding the selected category
-                    $link = 'index.php?option=com_osdownloads&view=item&id=' . $documentId;
-
-                    $query = $db->getQuery(true)
-                        ->update('#__menu')
-                        ->where('id = ' . (int)$menu->id)
-                        ->set('link = ' . $db->quote($link))
-                        ->set('params = ' . $db->quote($params->toString()));
-                    $db->setQuery($query)->execute();
-                }
-            }
-        }
-    }
-
-    /**
-     * Adjust component parameters as needed
-     *
-     * @TODO: Move to AbstractInstaller script
-     */
-    protected function checkParamStructure()
-    {
-        /** @var Extension $table */
-        $table = Table::getInstance('Extension');
-        $table->load(['element' => 'com_osdownloads', 'type' => 'component']);
-
-        $params  = new Registry($table->get('params'));
-        $current = $params->toObject();
-
-        $parameterMap = $this->getParameterChangeMap();
-        foreach ($parameterMap as $oldKey => $newKey) {
-            if ($value = $params->get($oldKey)) {
-                $params->set($newKey, $value);
-                $params->remove($oldKey);
-            }
-        }
-
-        if ($current != $params->toObject()) {
-            $table->params = $params->toString();
-            $table->store();
-        }
-    }
-
-    /**
-     * Return array mapping old Parameter keys to new
-     *
-     * @return array
-     */
-    protected function getParameterChangeMap(): array
-    {
-        return [
-            'connect_mailchimp' => 'mailinglist.mailchimp.enable',
-            'mailchimp_api'     => 'mailinglist.mailchimp.api',
-            'list_id'           => 'mailinglist.mailchimp.list_id'
-        ];
     }
 
     /**
@@ -359,21 +173,6 @@ class AbstractScript extends AllediaAbstractScript
     protected function fixDatabase()
     {
         $db = $this->dbo;
-
-        /*
-         * There is an odd issue in Joomla 4 that reports database errors
-         * when there aren't any. Stupid.
-         */
-        $oldUpdates = Folder::files(
-            JPATH_ADMINISTRATOR . '/components/com_osdownloads/sql/updates',
-            '1\.4\.9\.sql',
-            true,
-            true
-        );
-
-        foreach ($oldUpdates as $oldUpdate) {
-            File::delete($oldUpdate);
-        }
 
         /**
          * We moved to null dates as of v1.13.0
